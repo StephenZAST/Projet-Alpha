342bc4bd7de2eed0996c4d32379667fa5bec18e5
StephenZAST <contacteasytec@gmail.com>
2024-10-27 09:41:51 +0100

    api endpoint next

--- backend/src/utils/errors.ts
+++ backend/src/utils/errors.ts
@@ -1,14 +1,3 @@
-export class AppError extends Error {
-  constructor(
-    public statusCode: number,
-    public message: string,
-    public code?: string
-  ) {
-    super(message);
-    Object.setPrototypeOf(this, AppError.prototype);
-  }
-}
-
 export const errorCodes = {
   ARTICLE_NOT_FOUND: 'ARTICLE_NOT_FOUND',
   INVALID_ARTICLE_DATA: 'INVALID_ARTICLE_DATA',
