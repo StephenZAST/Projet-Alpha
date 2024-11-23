a77e7b17acf621786a307ce42c1dcb8f97bbfce7
StephenZAST <contacteasytec@gmail.com>
2024-11-23 04:41:00 +0100

    build(deps): update package-lock.json, App.jsx and main.jsx

--- frontend/web/admin/src/main.jsx
+++ frontend/web/admin/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { MaterialUIControllerProvider } from "./context";
+
+const container = document.getElementById("app");
+
+if (container) {
+  const root = createRoot(container);
+
+  root.render(
+    <BrowserRouter>
+      <MaterialUIControllerProvider>
+        <App />
+      </MaterialUIControllerProvider>
+    </BrowserRouter>
+  );
+}
