34622d07426ca684c5fb78eec394457b0fd489cb
StephenZAST <contacteasytec@gmail.com>
2024-11-27 06:16:27 +0100

    update 5 files

--- frontend/web/admin/src/components/Schedule.tsx
+++ frontend/web/admin/src/components/Schedule.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Calendar } from 'react-big-calendar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import moment from 'moment';
 import styles from './style/Schedule.module.css';
 
 const Schedule: React.FC = () => {
   return (
     <div className={styles.scheduleContainer}>
       <h2>Schedule</h2>
-      <Calendar />
+      <Calendar
+        localizer={momentLocalizer(moment)}
+        defaultDate={new Date()}
+        defaultView="month"
+      />
     </div>
   );
 };
