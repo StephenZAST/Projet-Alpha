809d7a9179be50e751bbf1b1bfa4fe9d0923c00d
StephenZAST <contacteasytec@gmail.com>
2024-11-29 01:43:14 +0100

    update Settings.tsx

---
+++ frontend/web/admin/src/layouts/MasterSuperAdminDashboard/views/Settings.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../redux/store';
+import styles from './Settings.module.css';
+
+export const Settings: React.FC = () => {
+  const user = useSelector((state: RootState) => state.auth.user);
+
+  return (
+    <div className={styles.settingsContainer}>
+      <section className={styles.profileSection}>
+        <div className={styles.profileCard}>
+          <h3>Informations Personnelles</h3>
+          <div className={styles.profileForm}>
+            <div className={styles.formGroup}>
+              <label>Email</label>
+              <input type="email" defaultValue={user?.email} />
+            </div>
+            <div className={styles.formGroup}>
+              <label>Téléphone</label>
+              <input type="tel" defaultValue={user?.phone} />
+            </div>
+            <div className={styles.formGroup}>
+              <label>Nom</label>
+              <input type="text" defaultValue={user?.name} disabled />
+            </div>
+            <div className={styles.formGroup}>
+              <label>Type Admin</label>
+              <input type="text" defaultValue="Master Super Admin" disabled />
+            </div>
+            <button className={styles.updateButton}>
+              Mettre à jour
+            </button>
+          </div>
+        </div>
+      </section>
+
+      <section className={styles.preferencesSection}>
+        <div className={styles.preferencesCard}>
+          <h3>Préférences</h3>
+          <div className={styles.preferencesList}>
+            <div className={styles.preferenceItem}>
+              <label>Notifications Email</label>
+              <input type="checkbox" defaultChecked />
+            </div>
+            <div className={styles.preferenceItem}>
+              <label>Notifications SMS</label>
+              <input type="checkbox" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
