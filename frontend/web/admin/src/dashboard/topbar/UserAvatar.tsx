c27ed4a1b23460f98bbb331f75992f6c6bec1e4b
StephenZAST <contacteasytec@gmail.com>
2024-12-04 09:07:18 +0100

    admin dashboard deletion restarting by master super admin folder creation

--- frontend/web/admin/src/dashboard/topbar/UserAvatar.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './TopBar.module.css';
-import { UserAvatarProps } from './types';
-
-export const UserAvatar: React.FC<UserAvatarProps> = ({ userName }) => (
-  <div className={styles.userGreeting}>
-    <h1 className={styles.greeting}>Hello {userName} 👋🏼,</h1>
-  </div>
-);
