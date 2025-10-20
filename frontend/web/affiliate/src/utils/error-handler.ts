90f11234a886ea7030e1cbbf23d2e7ee9ca562f8
StephenZAST <contacteasytec@gmail.com>
2025-10-20 03:55:51 +0200

    Refactor code structure for improved readability and maintainability

--- frontend/web/affiliate/src/utils/error-handler.ts
+++
@@ -1,7 +0,0 @@
-import { toast } from 'react-toastify';
-
-export const handleApiError = (error: any) => {
-  const message = error.response?.data?.error || 'Une erreur est survenue';
-  toast.error(message);
-  return Promise.reject(error);
-};
